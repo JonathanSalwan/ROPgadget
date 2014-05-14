485d16a7d7894b3ce6d40191ee0bdf82d005ac64
Francisco Falcon <fdfalcon@gmail.com>
2014-05-14 13:53:31 -0300

    Added the 'count' command to the console.

--- ropgadget/core.py
+++ ropgadget/core.py
@@ -299,3 +299,12 @@ class Core(cmd.Cmd):
         print "keyword  = with"
         print "!keyword = witout"
 
+
+    def do_count(self, s):
+        print "[*] %d loaded gadgets." % len(self.__gadgets)
+
+
+    def help_count(self):
+        print "Shows the number of loaded gadgets."
+
+
