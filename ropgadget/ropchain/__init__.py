8c4b35aca798d866d4682d6195c0d3d302f6009b
Zach Riggle <zach@riggle.me>
2014-07-02 13:59:24 -0400

    Add Python setup script

--- ropgadget/ropchain/__init__.py
+++ ropgadget/ropchain/__init__.py
@@ -2,14 +2,13 @@
 ## -*- coding: utf-8 -*-
 ##
 ##  Jonathan Salwan - 2014-05-13
-## 
+##
 ##  http://shell-storm.org
 ##  http://twitter.com/JonathanSalwan
-## 
+##
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
 ##  the Free Software  Foundation, either  version 3 of  the License, or
 ##  (at your option) any later version.
 ##
-
-pass
+import ropmaker,arch
