f2635e011b557ab686cb844b4a229abb57274e7e
Zach Riggle <zach@riggle.me>
2014-07-02 13:59:24 -0400

    Add Python setup script

--- ropgadget/loaders/__init__.py
+++ ropgadget/loaders/__init__.py
@@ -2,13 +2,13 @@
 ## -*- coding: utf-8 -*-
 ##
 ##  Jonathan Salwan - 2014-05-12 - ROPgadget tool
-## 
+##
 ##  http://twitter.com/JonathanSalwan
 ##  http://shell-storm.org/project/ROPgadget/
-## 
+##
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
 ##  the Free Software  Foundation, either  version 3 of  the License, or
 ##  (at your option) any later version.
-
+import elf, macho, pe, raw
 pass
