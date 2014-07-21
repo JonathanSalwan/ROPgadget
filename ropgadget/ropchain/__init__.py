ae59389a78963bc55da41d3f13b34ff63354a8eb
Jonathan Salwan <jonathan.salwan@shell-storm.org>
2014-07-21 21:31:18 +0200

    Revert "Add Python setup script"

--- ropgadget/ropchain/__init__.py
+++ ropgadget/ropchain/__init__.py
@@ -2,13 +2,14 @@
 ## -*- coding: utf-8 -*-
 ##
 ##  Jonathan Salwan - 2014-05-13
-##
+## 
 ##  http://shell-storm.org
 ##  http://twitter.com/JonathanSalwan
-##
+## 
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
 ##  the Free Software  Foundation, either  version 3 of  the License, or
 ##  (at your option) any later version.
 ##
-import ropmaker,arch
+
+pass
