de32074545703112c11697fc802b4b6add9bd1a6
Kevin Hamacher <kevin.hamacher@ruhr-uni-bochum.de>
2014-08-21 15:39:25 +0200

    Added support for x86 'call esp' instruction

--- ropgadget/gadgets.py
+++ ropgadget/gadgets.py
@@ -117,7 +117,7 @@ class Gadgets:
                                ["\xff[\x20\x21\x22\x23\x26\x27]{1}", 2, 1, self.__binary.getArch(), self.__binary.getArchMode()], # jmp  [reg]
                                ["\xff[\xe0\xe1\xe2\xe3\xe4\xe6\xe7]{1}", 2, 1, self.__binary.getArch(), self.__binary.getArchMode()], # jmp  [reg]
                                ["\xff[\x10\x11\x12\x13\x16\x17]{1}", 2, 1, self.__binary.getArch(), self.__binary.getArchMode()], # jmp  [reg]
-                               ["\xff[\xd0\xd1\xd2\xd3\xd6\xd7]{1}", 2, 1, self.__binary.getArch(), self.__binary.getArchMode()]  # jmp  [reg]
+                               ["\xff[\xd0\xd1\xd2\xd3\xd4\xd6\xd7]{1}", 2, 1, self.__binary.getArch(), self.__binary.getArchMode()]  # call  [reg]
                           ]
         gadgetsSparc    = [
                                ["\x81\xc0[\x00\x40\x80\xc0]{1}\x00", 4, 4, self.__binary.getArch(), CS_MODE_BIG_ENDIAN]  # jmp %g[0-3]
