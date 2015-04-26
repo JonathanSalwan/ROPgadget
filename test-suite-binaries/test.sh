ea5bee6f2268b318315aad531efbd5d39eb4fc36
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-04-26 15:15:32 +0200

    Fix test.sh file

--- test-suite-binaries/test.sh
+++ test-suite-binaries/test.sh
@@ -2,9 +2,9 @@
 FILES=`ls`
 for f in $FILES
 do
-  if [ "$f" != "test.sh" ]
+  if [ "$f" != "test.sh" ] && [ "$f" != "core" ]
   then
-    python ../ROPgadget.py --binary $f 1>/dev/null
+    python ../ROPgadget.py --depth 5 --binary $f 1>/dev/null
     echo "$f analyzed"
   fi
 done
