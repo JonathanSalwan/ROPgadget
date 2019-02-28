a5ab76a0d407c233832b0d6e8a59c61b1aefb846
Jonathan Salwan <jonathan.salwan@gmail.com>
2019-02-28 09:33:50 +0100

    Fix version 5.7

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-version = "5.6"
+version = "5.7"
 
 setup(
     name             = package_name,
