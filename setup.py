5fef7f53ea95233d6a2550e570b811069e425a96
Alexey Vishnyakov <vishnya@ispras.ru>
2020-01-21 16:02:43 +0300

    Add version 6.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-version = "5.9"
+version = "6.0"
 
 setup(
     name             = package_name,
