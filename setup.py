fe78cf2249caaa335e752e06589360049ffb5387
Alexey Vishnyakov <vishnya@ispras.ru>
2020-02-21 15:05:20 +0300

    Support Python3

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Intended Audience :: Developers'
     ]
 )
