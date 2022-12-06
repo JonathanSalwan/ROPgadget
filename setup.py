030b4d9b70df4fc975bd16d21cfc5866c4d836de
Alexey Vishnyakov <vishnya@ispras.ru>
2022-12-06 11:29:50 +0300

    Add version 7.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-version = "7.1"
+version = "7.2"
 
 setup(
     name             = package_name,
