0506961fc57b74bc27913b9aeb531c4b6c13f6f3
Mario Haustein <mario.haustein@hrz.tu-chemnitz.de>
2023-09-01 07:34:36 +0200

    Recognize `ret` and `jmp` with `repz` and `notrack` prefix as valid branching instructions

--- ropgadget/gadgets.py
+++ ropgadget/gadgets.py
@@ -31,7 +31,7 @@ class Gadgets(object):
         self.__filterRE = re.compile("({})$".format(re_str)) if re_str else None
 
     def __passCleanX86(self, decodes):
-        br = ["ret", "retf", "int", "sysenter", "jmp", "call", "syscall"]
+        br = ["ret", "repz ret", "retf", "int", "sysenter", "jmp", "notrack jmp", "call", "notrack call", "syscall"]
 
         if decodes[-1][2] not in br:
             return True
