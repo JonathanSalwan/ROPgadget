d313024e93e60c338e7755f6fd541094a5c40347
n3rada <72791564+n3rada@users.noreply.github.com>
2024-10-23 10:50:02 +0200

    [ROPMaker] Quick refactorisation

--- ropgadget/ropchain/ropmaker.py
+++ ropgadget/ropchain/ropmaker.py
@@ -6,35 +6,38 @@
 ##  http://twitter.com/JonathanSalwan
 ##
 
+# Third party library imports
 from capstone import *
 
-from ropgadget.ropchain.arch.ropmakerx64 import *
-from ropgadget.ropchain.arch.ropmakerx86 import *
+# Local library imports
+from ropgadget.ropchain.arch.ropmakerx64 import ROPMakerX64
+from ropgadget.ropchain.arch.ropmakerx86 import ROPMakerX86
 
 
-class ROPMaker(object):
+class ROPMaker:
     def __init__(self, binary, gadgets, offset):
-        self.__binary  = binary
-        self.__gadgets = gadgets
-        self.__offset  = offset
+        self._binary = binary
+        self._gadgets = gadgets
+        self._offset = offset
 
-        self.__handlerArch()
+        self._handler_arch()
 
-    def __handlerArch(self):
+    def _handler_arch(self):
+        # Define a mapping of (arch, mode, format) tuples to handler classes
+        arch_map = {
+            (CS_ARCH_X86, CS_MODE_32, "ELF"): ROPMakerX86,
+            (CS_ARCH_X86, CS_MODE_64, "ELF"): ROPMakerX64,
+        }
 
-        if (
-            self.__binary.getArch() == CS_ARCH_X86
-            and self.__binary.getArchMode() == CS_MODE_32
-            and self.__binary.getFormat() == "ELF"
-        ):
-            ROPMakerX86(self.__binary, self.__gadgets, self.__offset)
+        # Extract the current arch, mode, and format
+        arch_info = (self._binary.getArch(), self._binary.getArchMode(), self._binary.getFormat())
 
-        elif (
-            self.__binary.getArch() == CS_ARCH_X86
-            and self.__binary.getArchMode() == CS_MODE_64
-            and self.__binary.getFormat() == "ELF"
-        ):
-            ROPMakerX64(self.__binary, self.__gadgets, self.__offset)
+        # Get the corresponding handler class, if it exists
+        handler_class = arch_map.get(arch_info)
 
-        else:
-            print("\n[Error] ROPMaker.__handlerArch - Arch not supported yet for the rop chain generation")
+        if handler_class:
+            handler_class(self._binary, self._gadgets, self._offset)
+            return
+
+        print(f"\n[Error] ROPMaker - Architecture not supported: {arch_info} (CS_ARCH, CS_MODE, format)")
+
